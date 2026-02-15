fc6f50bfee7e841ed26f3f707359d6627ad2911e
sudo-kk <karthikvk380@gmail.com>
2026-02-15 20:25:10 +0530

    Coded to render OOPS as banner using print statements

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,12 @@
+// OOPSBannerAppUC2.java
+public class OOPSBannerAppUC2 {
+    public static void main(String[] args) {
+        System.out.println("  *****   *****   *******    ***** ");
+        System.out.println(" *     * *     *  *      *  *      ");
+        System.out.println("*       *       * *     *   *       ");
+        System.out.println("*       *       * ******     **** ");
+        System.out.println("*       *       * *              * ");
+        System.out.println(" *     * *     *  *              * ");
+        System.out.println("  *****   *****   *         ***** ");
+    }
+}
